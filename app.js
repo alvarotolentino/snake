0f0b88b1f50018298f7533940ce7f8d16252b8e5
Alvaro Tolentino <alvarotolentinou@gmail.com>
2020-04-09 16:51:39 -0500

    Fix and add readme

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const squares = document.querySelectorAll('.grid div');
-  const scoreDispay = document.querySelector('span');
+  const scoreDisplay = document.querySelector('span');
   const starBtn = document.querySelector('.start');
 
   const width = 10;
@@ -21,8 +21,8 @@ document.addEventListener('DOMContentLoaded', () => {
     score = 0;
     randomApple();
     direction = 1;
-    scoreDispay.innerText = score;
-    innerHeight = 1000;
+    scoreDisplay.innerText = score;
+    intervalTime = 1000;
     currentSnake = [2, 1, 0];
     currentIndex = 0;
     currentSnake.forEach((index) => squares[index].classList.add('snake'));
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
       currentSnake.push(tail);
       randomApple();
       score++;
-      scoreDispay.textContent = score;
+      scoreDisplay.textContent = score;
       clearInterval(interval);
       intervalTime = intervalTime * speed;
       interval = setInterval(moveOutcomes, intervalTime);
